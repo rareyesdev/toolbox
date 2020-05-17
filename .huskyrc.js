d362f7d122120abc7cedfadaabd52fd102d2b6d7
Ramón Alejandro Reyes Fajardo <rareyesdev@gmail.com>
2020-05-16 22:23:58 -0500

    chore(root): update configuration

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   hooks: {
-    "prepare-commit-msg": "if test \"$(git config --bool hooks.prepare-commit-msg.disabled)\" != true ; then exec < /dev/tty && npm run cm -- --hook || true ; fi",
-    "pre-commit": "lint-staged",
+    'prepare-commit-msg': 'if test "$(git config --bool hooks.prepare-commit-msg.disabled)" != true ; then exec < /dev/tty && npm run cm -- --hook || true ; fi',
+    'pre-commit': 'lint-staged',
   },
 }
