bae1eee96b4803180bb1683e01c14d1168d670f7
Ramón Alejandro Reyes Fajardo <rareyesdev@gmail.com>
2020-05-24 18:23:11 -0500

    docs(root): add comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,4 @@
+// We reference @rareyes/eslint-config in package.json to get all dependencies but we reference the config file
+// directly to always lint with the latest (not released) version of @rareyes/eslint-config.
+// This allows @rareyes/eslint-config to lint itself.
 module.exports = require('./packages/eslint-config');
