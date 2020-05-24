c5ca977c7a6e10ad955d1916d4adf29c83ea0652
Ramón Alejandro Reyes Fajardo <rareyesdev@gmail.com>
2020-05-24 18:56:53 -0500

    docs(async-delay): add documentation

--- packages/library/src/async-delay.ts
+++ packages/library/src/async-delay.ts
@@ -1,3 +1,8 @@
+/**
+ * @param ms The amount of milliseconds before resolving.
+ * @param result An optional value to return.
+ * @returns A promise that will resolve after `ms` milliseconds with `result` if it was provided.
+ */
 export function asyncDelay<TResult>(ms: number, result?: TResult) {
   return new Promise<TResult>((resolve) => globalThis.setTimeout(() => resolve(result), ms));
 }
